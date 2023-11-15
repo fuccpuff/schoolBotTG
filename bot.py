0b5b58fda91a6456b4f9dfd85a3c6e7a2392e7b1
fuccpuff <fuccpuff@gmail.com>
2023-11-15 09:28:03 +0500

    edit code

--- bot.py
+++ bot.py
@@ -3,40 +3,57 @@ from telebot import types
 from config import BOT_TOKEN
 import database
 
-bot = telebot.TeleBot(BOT_TOKEN)
+bot = telebot.TeleBot(BOT_TOKEN)  # инициализирую бота с токеном из файла конфигурации
+
+# словарь для отслеживания состояний пользователей
+user_states = {}
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    user = database.get_user_by_chat_id(message.chat.id)
+    chat_id = message.chat.id
+    user = database.get_user_by_chat_id(chat_id)  # проверяю, есть ли пользователь в базе данных
     if user:
-        bot.send_message(message.chat.id, "Ты уже зарегистрирован.")
+        bot.send_message(chat_id, "Ты уже зарегистрирован.")
+        print(f"Пользователь {chat_id} уже зарегистрирован")  # добавляю отладочное сообщение
     else:
-        bot.send_message(message.chat.id, "Добро пожаловать! Давай зарегистрируем тебя. Как тебя зовут?")
-        bot.register_next_step_handler(message, process_name_step)
+        user_states[chat_id] = 'AWAITING_NAME'  # устанавливаю состояние пользователя на ожидание имени
+        bot.send_message(chat_id, "Добро пожаловать! Давай зарегистрируем тебя. Как тебя зовут?")
+        print(f"Запрашиваю имя у пользователя {chat_id}")  # добавляю отладочное сообщение
+        bot.register_next_step_handler(message, process_name_step)  # перехожу к следующему шагу
 
 def process_name_step(message):
     chat_id = message.chat.id
-    name = message.text
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
-    class_buttons = [types.KeyboardButton(f'{i}') for i in range(5, 12)]
-    markup.add(*class_buttons)
-    msg = bot.reply_to(message, 'В каком ты классе?', reply_markup=markup)
-    bot.register_next_step_handler(msg, process_class_step, name)
+    if user_states.get(chat_id) == 'AWAITING_NAME':
+        name = message.text  # сохраняю имя пользователя
+        user_states[chat_id] = 'AWAITING_CLASS'  # меняю состояние пользователя на ожидание класса
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+        class_buttons = [types.KeyboardButton(f'{i}') for i in range(5, 12)]  # создаю кнопки для выбора класса
+        markup.add(*class_buttons)
+        msg = bot.reply_to(message, 'В каком ты классе?', reply_markup=markup)
+        print(f"Запрашиваю класс у пользователя {chat_id}")  # добавляю отладочное сообщение
+        bot.register_next_step_handler(msg, process_class_step, name)
 
 def process_class_step(message, name):
-    class_number = message.text
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
-    class_letters = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж']
-    for letter in class_letters:
-        markup.add(types.KeyboardButton(letter))
-    msg = bot.reply_to(message, 'Какая буква твоего класса?', reply_markup=markup)
-    bot.register_next_step_handler(msg, process_class_letter_step, name, class_number)
+    chat_id = message.chat.id
+    if user_states.get(chat_id) == 'AWAITING_CLASS':
+        class_number = message.text  # сохраняю номер класса
+        user_states[chat_id] = 'AWAITING_CLASS_LETTER'  # меняю состояние пользователя на ожидание буквы класса
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+        class_letters = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж']  # создаю кнопки для выбора буквы класса
+        for letter in class_letters:
+            markup.add(types.KeyboardButton(letter))
+        msg = bot.reply_to(message, 'Какая буква твоего класса?', reply_markup=markup)
+        print(f"Запрашиваю букву класса у пользователя {chat_id}")  # добавляю отладочное сообщение
+        bot.register_next_step_handler(msg, process_class_letter_step, name, class_number)
 
 def process_class_letter_step(message, name, class_number):
-    class_letter = message.text
     chat_id = message.chat.id
-    database.add_user((chat_id, name, class_number, class_letter))
-    bot.send_message(chat_id, f'Ты успешно зарегистрирован: {name}, класс {class_number}{class_letter}')
+    if user_states.get(chat_id) == 'AWAITING_CLASS_LETTER':
+        class_letter = message.text  # сохраняю букву класса
+        database.add_user((chat_id, name, class_number, class_letter))  # добавляю пользователя в базу данных
+        bot.send_message(chat_id, f'Ты успешно зарегистрирован: {name}, класс {class_number}{class_letter}')
+        print(f"Пользователь {chat_id} зарегистрирован как {name}, класс {class_number}{class_letter}")  # добавляю отладочное сообщение
+        del user_states[chat_id]  # удаляю состояние пользователя после завершения регистрации
 
-# Главный цикл для запуска бота
+# главный цикл для запуска бота
 bot.polling(none_stop=True)
--- database.py
+++ database.py
@@ -1,56 +1,55 @@
 import sqlite3
 
 def create_connection():
-    """ Создать новое соединение с SQLite базой данных """
+    # пытаюсь создать соединение с базой данных sqlite
     try:
-        conn = sqlite3.connect("school_bot.db")
-        return conn
+        conn = sqlite3.connect("school_bot.db")  # указываю название файла базы данных
+        return conn  # возвращаю объект соединения
     except Exception as e:
-        print(e)
+        print("Ошибка при создании соединения с базой данных:", e)  # печатаю ошибку, если она возникает
 
 def add_user(user):
-    """ Добавить нового пользователя в базу данных """
-    conn = create_connection()
+    # функция для добавления нового пользователя в базу данных
+    conn = create_connection()  # создаю новое соединение с базой данных
     try:
         sql = ''' INSERT INTO users(chat_id, name, class_number, class_letter)
-                  VALUES(?,?,?,?) '''
-        cur = conn.cursor()
-        cur.execute(sql, user)
-        conn.commit()
+                  VALUES(?,?,?,?) '''  # sql запрос для вставки данных пользователя
+        cur = conn.cursor()  # создаю курсор для выполнения запросов
+        cur.execute(sql, user)  # выполняю запрос с данными пользователя
+        conn.commit()  # подтверждаю изменения в базе данных
     except Exception as e:
-        print(e)
+        print("Ошибка при добавлении пользователя:", e)  # печатаю ошибку, если она возникает
     finally:
-        conn.close()
+        conn.close()  # закрываю соединение с базой данных
 
 def get_user_by_chat_id(chat_id):
-    """ Получить пользователя по его chat_id """
-    conn = create_connection()
+    # функция для получения пользователя по его chat_id
+    conn = create_connection()  # создаю новое соединение с базой данных
     try:
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM users WHERE chat_id=?", (chat_id,))
-        return cur.fetchone()
+        cur = conn.cursor()  # создаю курсор
+        cur.execute("SELECT * FROM users WHERE chat_id=?", (chat_id,))  # выполняю запрос на поиск пользователя
+        return cur.fetchone()  # возвращаю результат запроса (первую найденную запись)
     except Exception as e:
-        print(e)
+        print("Ошибка при поиске пользователя:", e)  # печатаю ошибку, если она возникает
     finally:
-        conn.close()
+        conn.close()  # закрываю соединение с базой данных
 
-# Инициализация базы данных и создание таблицы
 def initialize_db():
-    conn = create_connection()
+    # функция для инициализации базы данных и создания таблицы пользователей
+    conn = create_connection()  # создаю соединение с базой данных
     sql_create_users_table = """ CREATE TABLE IF NOT EXISTS users (
                                     id integer PRIMARY KEY,
                                     chat_id integer NOT NULL,
                                     name text NOT NULL,
                                     class_number text,
                                     class_letter text
-                                ); """
+                                ); """  # sql запрос для создания таблицы пользователей
     try:
-        c = conn.cursor()
-        c.execute(sql_create_users_table)
+        c = conn.cursor()  # создаю курсор
+        c.execute(sql_create_users_table)  # выполняю запрос на создание таблицы
     except Exception as e:
-        print(e)
+        print("Ошибка при создании таблицы пользователей:", e)  # печатаю ошибку, если она возникает
     finally:
-        conn.close()
+        conn.close()  # закрываю соединение с базой данных
 
-# Вызов функции инициализации при запуске
-initialize_db()
+initialize_db()  # вызываю функцию инициализации базы данных при запуске скрипта
